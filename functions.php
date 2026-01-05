58899d608852bfb1c36fd2982a4a3d02da55cd70
GeorgeHCook <georgehhcook@gmail.com>
2026-01-05 15:05:08 +0000

    adding post type clients

--- functions.php
+++ functions.php
@@ -54,3 +54,62 @@ function dt_acf_json_load_point($paths) {
     return $paths;
 }
 add_filter('acf/settings/load_json', 'dt_acf_json_load_point');
+
+/**
+ * Register Clients Post Type
+ */
+function dt_register_clients_post_type() {
+    $labels = array(
+        'name'                  => 'Clients',
+        'singular_name'        => 'Client',
+        'menu_name'             => 'Clients',
+        'name_admin_bar'        => 'Client',
+        'archives'              => 'Client Archives',
+        'attributes'            => 'Client Attributes',
+        'parent_item_colon'     => 'Parent Client:',
+        'all_items'             => 'All Clients',
+        'add_new_item'          => 'Add New Client',
+        'add_new'               => 'Add New',
+        'new_item'              => 'New Client',
+        'edit_item'             => 'Edit Client',
+        'update_item'           => 'Update Client',
+        'view_item'             => 'View Client',
+        'view_items'            => 'View Clients',
+        'search_items'          => 'Search Client',
+        'not_found'             => 'Not found',
+        'not_found_in_trash'    => 'Not found in Trash',
+        'featured_image'        => 'Featured Image',
+        'set_featured_image'    => 'Set featured image',
+        'remove_featured_image' => 'Remove featured image',
+        'use_featured_image'    => 'Use as featured image',
+        'insert_into_item'      => 'Insert into client',
+        'uploaded_to_this_item' => 'Uploaded to this client',
+        'items_list'            => 'Clients list',
+        'items_list_navigation' => 'Clients list navigation',
+        'filter_items_list'     => 'Filter clients list',
+    );
+    
+    $args = array(
+        'label'                 => 'Client',
+        'description'           => 'Client post type',
+        'labels'                => $labels,
+        'supports'              => array('title', 'editor', 'thumbnail'),
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 20,
+        'menu_icon'             => 'dashicons-groups',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'      => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'post',
+        'show_in_rest'          => true,
+    );
+    
+    register_post_type('clients', $args);
+}
+add_action('init', 'dt_register_clients_post_type', 0);
